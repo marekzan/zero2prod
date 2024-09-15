0731a2e957492fb15c1440f069478166b5ed25d2
marekzan <marekzan25@gmail.com>
2024-09-15 21:37:11 +0200

    fix: using init_db script from source repo

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,84 +1,70 @@
 #!/usr/bin/env bash
+set -x
+set -eo pipefail
 
-set +x           # debug mode running. prints all commands before running them -x. +x to turn off
-set -eo pipefail # exit on error -e, exit on error in pipe -o pipefail. this ensures that the script will exit if any command fails
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error: sqlx is not installed."
+  echo >&2 "Use:"
+  echo >&2 "    cargo install --version='~0.8' sqlx-cli --no-default-features --features rustls,postgres"
+  echo >&2 "to install it."
+  exit 1
+fi
 
-function check_for_mandatory_commands {
-  if ! [ -x "$(command -v psql)" ]; then
-    echo >&2 "Error: psql is not installed."
+# Check if a custom parameter has been set, otherwise use default values
+DB_PORT="${DB_PORT:=5432}"
+SUPERUSER="${SUPERUSER:=postgres}"
+SUPERUSER_PWD="${SUPERUSER_PWD:=password}"
+APP_USER="${APP_USER:=app}"
+APP_USER_PWD="${APP_USER_PWD:=secret}"
+APP_DB_NAME="${APP_DB_NAME:=newsletter}"
+
+# Allow to skip Docker if a dockerized Postgres database is already running
+if [[ -z "${SKIP_DOCKER}" ]]; then
+  # if a postgres container is running, print instructions to kill it and exit
+  RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
+  if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
+    echo >&2 "there is a postgres container already running, kill it with"
+    echo >&2 "    docker kill ${RUNNING_POSTGRES_CONTAINER}"
     exit 1
   fi
-  if ! [ -x "$(command -v sqlx)" ]; then
-    echo >&2 "Error: sqlx is not installed."
-    echo >&2 "Use:"
-    echo >&2 "cargo install --version='~0.6' sqlx-cli --no-default-features --features rustls,postgres"
-    echo >&2 "to install it."
-    exit 1
-  fi
-
-}
-
-function set_env_vars {
-  # Check if a custom user has been set, otherwise default to 'postgres'
-  DB_USER=${POSTGRES_USER:=postgres}
-  DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-  DB_NAME="${POSTGRES_DB:=newsletter}"
-  DB_PORT="${POSTGRES_PORT:=5432}"
-  DB_HOST="${POSTGRES_HOST:=localhost}"
-}
-
-function dev_db_is_running {
-  local container_name="zero2prod_dev_db"
-
-  if docker ps --filter "name=$container_name" --quiet | grep -q .; then
-    echo "Container '$container_name' is running."
-    return 0
-  else
-    echo "Container '$container_name' is not running."
-    return 1
-  fi
-}
-
-function run_dev_db_if_not_running {
-  if ! dev_db_is_running || [ -z "${SKIP_DOCKER}" ]; then
-    docker run \
-      --name zero2prod_dev_db \
-      -e POSTGRES_USER=${DB_USER} \
-      -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-      -e POSTGRES_DB=${DB_NAME} \
-      -p "${DB_PORT}":5432 \
-      -d postgres \
-      postgres -N 1000
+  CONTAINER_NAME="postgres_$(date '+%s')"
+  # Launch postgres using Docker
+  docker run \
+    --env POSTGRES_USER=${SUPERUSER} \
+    --env POSTGRES_PASSWORD=${SUPERUSER_PWD} \
+    --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
+    --health-interval=1s \
+    --health-timeout=5s \
+    --health-retries=5 \
+    --publish "${DB_PORT}":5432 \
+    --detach \
+    --name "${CONTAINER_NAME}" \
+    postgres -N 1000
   # ^ Increased maximum number of connections for testing purposes
-  fi
 
-}
-
-function wait_for_postgres {
-  # Keep pinging Postgres until it's ready to accept commands
-  export PGPASSWORD="${DB_PASSWORD}"
-  until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  until [ \
+    "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
+    "healthy" \
+    ]; do
     >&2 echo "Postgres is still unavailable - sleeping"
     sleep 1
   done
-  >&2 echo "Postgres is up and running on port ${DB_PORT}!"
-}
 
-function configure_dev_db {
-  DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
-  export DATABASE_URL
-  sqlx database create
-  sqlx migrate run
+  # Create the application user
+  CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
+  docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}"
 
-  >&2 echo "Postgres has been migrated, ready to go!"
-}
+  # Grant create db privileges to the app user
+  GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
+  docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
+fi
 
-function main {
-  check_for_mandatory_commands
-  set_env_vars
-  run_dev_db_if_not_running
-  wait_for_postgres
-  configure_dev_db
-}
+>&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
-main
+# Create the application database
+DATABASE_URL=postgres://${APP_USER}:${APP_USER_PWD}@localhost:${DB_PORT}/${APP_DB_NAME}
+export DATABASE_URL
+sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
